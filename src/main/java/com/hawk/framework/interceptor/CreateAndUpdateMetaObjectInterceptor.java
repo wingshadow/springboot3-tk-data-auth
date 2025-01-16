9617f06529698a23ccb207c8ce4f856bd2a2d4aa
zhangbin <zhangqiyi1@163.com>
2025-01-16 13:19:16 +0800

    1

--- src/main/java/com/hawk/framework/interceptor/CreateAndUpdateMetaObjectInterceptor.java
+++ src/main/java/com/hawk/framework/interceptor/CreateAndUpdateMetaObjectInterceptor.java
@@ -7,6 +7,7 @@ import com.hawk.framework.exception.ServiceException;
 import com.hawk.framework.helper.LoginHelper;
 import com.hawk.framework.model.LoginUser;
 import com.hawk.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
@@ -16,6 +17,7 @@ import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,6 +26,7 @@ import java.util.Map;
  * @author: zhb
  * @create: 2024-12-03 15:28
  */
+@Slf4j
 @Intercepts({
         @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
 })
@@ -42,6 +45,7 @@ public class CreateAndUpdateMetaObjectInterceptor implements Interceptor {
                 fillFields(metaObject, false);
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new ServiceException("自动填充异常 => " + e.getMessage(), HttpStatus.HTTP_UNAUTHORIZED);
         }
 
@@ -57,18 +61,46 @@ public class CreateAndUpdateMetaObjectInterceptor implements Interceptor {
     private void fillFields(Object metaObject, boolean isInsert) {
         if (metaObject instanceof Map) {
             Map<?, ?> paramMap = (Map<?, ?>) metaObject;
-            metaObject = paramMap.get("record");
-        }
 
+            // 遍历 keySet，获取所有的键
+            for (Object key : paramMap.keySet()) {
+                Object value = paramMap.get(key);
+                // 如果需要，根据 key 进一步处理 value
+                if ("record".equals(key)) {
+                    // 如果找到 "record" 键，处理逻辑
+                    Object record = value;
+                    fillSingleEntity(record, isInsert);
+                } else if ("recordList".equals(key)) {
+                    // 如果是 "recordList" 键，处理集合
+                    Object recordList = value;
+                    if (recordList instanceof List) {
+                        for (Object item : (List<?>) recordList) {
+                            fillSingleEntity(item, isInsert);
+                        }
+                    }
+                }
+            }
+
+            return; // 结束当前方法执行
+        }
+        fillSingleEntity(metaObject,isInsert);
+    }
+
+    /**
+     * 处理单个实体对象的填充逻辑
+     */
+    private void fillSingleEntity(Object metaObject, boolean isInsert) {
         if (metaObject instanceof BaseDataEntity) {
             BaseDataEntity entity = (BaseDataEntity) metaObject;
             Date current = new Date();
 
             if (isInsert) {
+                // 插入操作：设置 createTime 和 createBy
                 entity.setCreateTime(ObjectUtil.isNotNull(entity.getCreateTime()) ? entity.getCreateTime() : current);
                 entity.setCreateBy(StringUtils.isNotBlank(entity.getCreateBy()) ? entity.getCreateBy() : getLoginUsername());
             }
 
+            // 无论是插入还是更新操作，设置 updateTime 和 updateBy
             entity.setUpdateTime(current);
             entity.setUpdateBy(StringUtils.isNotBlank(entity.getUpdateBy()) ? entity.getUpdateBy() : getLoginUsername());
         }
